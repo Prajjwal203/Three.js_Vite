6742952d41d0d412249e6612e53d12ab689ae21b
Prajjwal Singh <prajjwals203@gmail.com>
2024-11-09 14:29:33 +0530

    Initital commit for Three.js Vite project

---
+++ main.js
@@ -0,0 +1,15 @@
+import * as THREE from "three";
+
+const canvas = document.getElementById('canvas');
+
+// 1. Create the Scene:
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('#F0F0F0');
+
+// 2. Camera:
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// 3. Object:
+const geometry = new THREE.DodecahedronGeometry();
+const material = new THREE.MeshLambertMaterial( { color: '#468585', emmisive: '#468585' })
